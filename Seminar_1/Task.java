9b03208ba16130cb3d5830e63651f2f77ca47a62
Ilya.Kolotov <kolotov.iljya@gmail.com>
2023-02-16 11:45:14 +0500

    Добавлена функция вычисления треугольного числа

--- Seminar_1/Task.java
+++ Seminar_1/Task.java
@@ -1,11 +1,16 @@
-import java.util.Scanner;
 package Seminar_1;
-
+import java.util.Scanner;
 public class Task {
 
     public static int getData(String text) { 
         System.out.print(text); 
         return input.nextInt(); 
+    }
+
+
+    public static int triangNumber(int num) { 
+        int result = (num*(num+1))/2;
+        return result;
     }
 
 }
