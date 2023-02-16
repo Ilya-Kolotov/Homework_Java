2ca8a09e48c0ee032a0d3b37bf4cf7e22518db44
Ilya.Kolotov <kolotov.iljya@gmail.com>
2023-02-16 11:41:29 +0500

    Добавлена функция получения данных от пользователя

---
+++ Seminar_1/Task.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+package Seminar_1;
+
+public class Task {
+
+    public static int getData(String text) { 
+        System.out.print(text); 
+        return input.nextInt(); 
+    }
+
+}
