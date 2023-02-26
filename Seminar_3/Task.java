32f99d00475baa47c9b802aca61802f534bad29f
Ilya.Kolotov <kolotov.iljya@gmail.com>
2023-02-26 19:07:55 +0500

    Add Merge Sort

--- Seminar_3/Task.java
+++ Seminar_3/Task.java
@@ -1,5 +1,6 @@
 package Seminar_3;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Task {
@@ -12,5 +13,38 @@ public class Task {
         IntStream.range(0, size)
                 .forEach(index -> array[index] = (int) (Math.random() * 2 * range) - range);
         return array;
+    }
+    public static int[] mergeSort(int[] array) {
+        int[] copyArray = Arrays.copyOf(array, array.length);
+        int[] filledArray = new int[array.length];
+        int[] result = mergeSortInternal(copyArray, filledArray, 0, array.length);
+        return result;
+    }
+
+    public static int[] mergeSortInternal(int[] copyArray, int[] filledArray, int startIndex, int endIndex) {
+        if (startIndex >= endIndex - 1) {
+            return copyArray;
+        }
+
+        int middle = startIndex + (endIndex - startIndex) / 2;
+        int[] sorted1 = mergeSortInternal(copyArray, filledArray, startIndex, middle);
+        int[] sorted2 = mergeSortInternal(copyArray, filledArray, middle, endIndex);
+
+        int index1 = startIndex;
+        int index2 = middle;
+        int destIndex = startIndex;
+        int[] result = sorted1 == copyArray ? filledArray : copyArray;
+        while (index1 < middle && index2 < endIndex) {
+            result[destIndex++] = sorted1[index1] < sorted2[index2]
+                    ? sorted1[index1++]
+                    : sorted2[index2++];
+        }
+        while (index1 < middle) {
+            result[destIndex++] = sorted1[index1++];
+        }
+        while (index2 < endIndex) {
+            result[destIndex++] = sorted2[index2++];
+        }
+        return result;
     }
 }
